11430ec240db199157db0fe48a9610de5a3f6fdb
Søren Louv-Jansen <sorenlouv@gmail.com>
2019-01-04 00:58:34 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+// test url: https://fantasy.premierleague.com/drf/leagues-classic-standings/464499
+httpProxy
+  .createProxyServer({
+    target: 'https://fantasy.premierleague.com',
+    changeOrigin: true
+  })
+  .listen(3000);
+
+console.log('Proxying on 3000');
