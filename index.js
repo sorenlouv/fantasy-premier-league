9407fc93b1152db00d0345119903d0a68568fc59
Søren Louv-Jansen <sorenlouv@gmail.com>
2019-01-11 23:30:48 +0100

    Add CORS

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 
 // test url: https://fantasy.premierleague.com/drf/leagues-classic-standings/464499
-httpProxy
+const proxy = httpProxy
   .createProxyServer({
-    target: 'https://fantasy.premierleague.com',
+    target: 'https://fantasy.premierleague.com/drf',
     changeOrigin: true
   })
   .listen(3000);
+
+  proxy.on('proxyRes', (proxyRes, req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  });
+
 
 console.log('Proxying on 3000');
